b53f3277b524f406c345511d9bf3df1714677213
Avneet <avneetmaankiya@gmail.com>
2021-08-31 05:51:57 +0530

    app bar change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:radio_ai/utils/ai_util.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +24,20 @@ class HomePage extends StatelessWidget {
           VxAnimatedBox()
               .size(context.screenWidth, context.screenHeight)
               .withGradient(LinearGradient(
-                  colors: [AiColors.primartColor2, AiColors.primaryColor1]))
+                  colors: [AiColors.primaryColor1, AiColors.primartColor2],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight))
               .make(),
+          AppBar(
+            title: "AI Radio"
+                .text
+                .xl4
+                .bold
+                .make()
+                .shimmer(primaryColor: Vx.purple300, secondaryColor: Vx.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          )
         ],
       ),
     );
